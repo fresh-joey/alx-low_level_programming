eadeb9e0154d1ad1e3c429da79274839220d1d8c
fresh-joey <johserf30@gmail.com>
2022-09-17 13:50:06 -0700

    name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-# #!/bin/bash
+# !/bin/bash
 #include <stdio.h>
 /**
  *main - main block
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts('\n'"Programming is like building a multilingual puzzle"); 
+	puts('\n'"Programming is like building a multilingual puzzle");
 	return (0);
 }
