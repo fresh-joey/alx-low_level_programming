a6f021e9dea56ff9b96cdf32e38ef941ed86d74f
fresh-joey <johserf30@gmail.com>
2022-09-17 11:05:14 -0700

    put C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-# !/bin/bash
+# #!/bin/bash
 #include <stdio.h>
-
 /**
  *main - main block
  *Return: 0
  */
-
 int main(void)
 {
-	puts(\n"Programming is like building a multilingual puzzle"); 
+	puts('\n'"Programming is like building a multilingual puzzle"); 
 	return (0);
 }
