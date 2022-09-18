d3a82c842491754adc82257ccd0a535ed6f113e4
fresh-joey <johserf30@gmail.com>
2022-09-18 10:03:41 -0700

    print alphabet with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: Use `putchar` to print all the alphabet except q and e
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar (c);
+		c++;
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
