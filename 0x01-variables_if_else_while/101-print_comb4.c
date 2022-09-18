c69fd6c32d5d8510406503e67531ff67cd179158
fresh-joey <johserf30@gmail.com>
2022-09-18 13:00:34 -0700

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: Print all possible different combination of three digits
+ *
+ * the three digits must be different
+ *
+ * print only the smallest combination of three digits and in ascending order
+ *
+ * you can only use `putchar` function and a maximum of six times
+ *
+ * you are not allowed to use any variable of type char
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 8; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 9; num2++)
+		{
+			for (num3 = num2 + 1; num3 < 10; num3++)
+			{
+				putchar ((num2 % 10) + '0');
+				putchar ((num3 % 10) + '0');
+
+				if (num1 == 7 && num2 == 8 && num3 == 9)
+					continue;
+
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
+
+
