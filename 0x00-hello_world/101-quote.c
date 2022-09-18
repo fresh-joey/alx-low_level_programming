9a20592585d1157a975d2de345a322cfc7800991
fresh-joey <johserf30@gmail.com>
2022-09-18 07:53:19 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,10 +10,10 @@
  */
 int main(void)
 {
-	int S = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
 	write(2,
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		S);
+		s);
 	return (1);
 }
