cdcb97a6a2423b3207299db661827f82987c8be8
fresh-joey <johserf30@gmail.com>
2022-09-21 15:46:09 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,10 +10,10 @@ void rev_string(char *s)
 	int i, c, l;
 	char h;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		;
 	l = i;
-	for(i--, c = 0; c < l/2; i--, c++)
+	for (i--, c = 0; c < l / 2; i--, c++)
 	{
 		h = s[c];
 		s[c] = s[i];
