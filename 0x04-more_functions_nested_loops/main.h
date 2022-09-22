d13804513b3ab3e9149cf056b7ccb4f72df1b7f1
fresh-joey <johserf30@gmail.com>
2022-09-22 12:51:02 -0700

    uppercase character

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef main_H
+#define main_H
 
 /*
  * Desc: Header file containing prototypes for all functions
@@ -8,27 +8,16 @@
  *
  */
 int _putchar(char c);
-
 int _isupper(int c);
-
 int _isdigit(int c);
-
 int mul(int a, int b);
-
 void print_numbers(void);
-
 void print_most_numbers(void);
-
 void more_numbers(void);
-
 void print_line(int n);
-
 void print_diagonal(int n);
-
 void print_square(int size);
-
 void print_triangle(int size);
-
 void print_number(int n);
 
 
